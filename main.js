618027901eeb3392c19bd94209446b7279c88d3f
kanishkachandrakar <122943743+kanishkachandrakar@users.noreply.github.com>
2024-03-24 01:17:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/*--------------typed js-------------------*/
+const typed = new Typed('.multiple-text',{
+    strings: ['Software Engineer','Web Designer','Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay:1000,
+    loop: true,
+});
