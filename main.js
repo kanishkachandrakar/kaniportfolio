4530388c2fb29d7eb512d53e1f09049e65994fe5
Kanishka Chandrakar <122943743+kanishkachandrakar@users.noreply.github.com>
2026-01-10 17:54:51 -0500

    new website design

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 /*--------------typed js-------------------*/
 const typed = new Typed('.multiple-text',{
-    strings: ['Software Engineer','Web Designer','Frontend Developer'],
+    strings: ['Software Engineer 🤖','Fun-time Web Designer 👩🏻‍💻','Vibe Coder 🎉'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay:1000,
